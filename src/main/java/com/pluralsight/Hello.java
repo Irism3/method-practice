6bc0668fc5851775ac058f62f7d7a444b4e5b2d3
irismelendez <iris.13melendez@gmail.com>
2025-09-26 18:27:01 -0500

    method practice

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -1,4 +1,16 @@
 package com.pluralsight;
 
+import java.awt.datatransfer.StringSelection;
+
 public class Hello {
+    public static void main(String[] args) {
+        String sayHello;
+        String sayGoodbye;
+        String sayGoodMorning;
+
+        System.out.println( );
+
+
+
+    }
 }
