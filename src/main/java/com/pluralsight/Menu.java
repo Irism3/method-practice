808f279d23e99a4aecd25c48b47d0f7a915e2618
irismelendez <iris.13melendez@gmail.com>
2025-09-26 19:53:14 -0500

    i created a menu method

---
+++ src/main/java/com/pluralsight/Menu.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Menu {
+    public static void main(String[] args) {
+        displayMenu();
+        System.out.println();  //calling it twice
+        displayMenu();
+
+    }
+
+    public static void displayMenu() {
+        System.out.println("===MENU===");
+        System.out.println("1. Coffee - $3.99");
+        System.out.println("2. Tea - $2.99");
+        System.out.println("3. Cookie - $1.99");
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
