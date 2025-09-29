1a9afc7727e63a7de87215056f1e83ec3b35fd66
irismelendez <iris.13melendez@gmail.com>
2025-09-29 00:40:30 -0500

    i created compared numbers

--- src/main/java/com/pluralsight/CompareNumbers.java
+++ src/main/java/com/pluralsight/CompareNumbers.java
@@ -1,5 +1,21 @@
 package com.pluralsight;
 
 public class CompareNumbers {
+    public static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    public static boolean isPositive(double number) {
+        return number >0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+        System.out.println(isEven(44));
+        System.out.println(isPositive(2.4));
+        System.out.println(isPositive(-13.4));
+    }
+
+
 
 }
